afab929bb69ef1b7976e3c2714ebdf4223f1323d
gufranalam226 <63334077+gufranalam226@users.noreply.github.com>
2025-07-10 10:27:57 +0530

    final update

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require(`express`);
 const Routes = express.Router();
 const Article= require(`../model/article`);
@@ -51,57 +50,3 @@ Routes.get(`/delete/:id`, async (req, res) => {
 
 
 module.exports = Routes;
-=======
-const express = require(`express`);
-const Routes = express.Router();
-const Article= require(`../model/article`);
-
-
-Routes.get(`/new`, (req, res)=>{
-    res.render(`article/article`);
-});
-
-Routes.get(`/:slug` , async (req, res)=>{
-    const article = await Article.findOne({slug:req.params.slug});
-    if(article==null){res.redirect(`/`)}
-    res.render(`article/show`, {article:article})
-
-})
-
-Routes.post(`/`, (req, res)=>{
-    console.log("working");
-    const article = new Article({
-        title:req.body.title,
-        content:req.body.content,
-        additionalInfo:req.body.additionalInfo
-    })
-    article.save().then(()=>{
-        res.redirect(`/`)
-    })
-})
-
-// Routes.get(`/delete/:id`, (req, res)=>{
-//     Article.findByIdAndDelete({_id:req.params.id}, (err)=>{
-//     if(err){ res.send(`Sorry`)}
-//     else{res.redirect(`/`)}
-//     })
-// })
-
-Routes.get(`/delete/:id`, async (req, res) => {
-    try {
-        const deletedArticle = await Article.findByIdAndDelete(req.params.id).exec();
-        
-        if (!deletedArticle) {
-            return res.status(404).send(`Article not found`);
-        }
-
-        res.redirect(`/`);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(`Internal Server Error`);
-    }
-});
-
-
-module.exports = Routes;
->>>>>>> 43adb13c4697aa44dea742af59a456607d7286f8
